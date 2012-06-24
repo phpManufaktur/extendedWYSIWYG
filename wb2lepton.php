803d29fc748940a5ad795cf2ec242a7378787eaa
hertsch <ralf.hertsch@phpmanufaktur.de>
2012-06-24 19:37:37 +0200

    Added wb2lepton.php

---
+++ wb2lepton.php
@@ -0,0 +1,13 @@
+<?php
+
+/**
+ * extendedWYSIWYG
+ *
+ * @author Ralf Hertsch <ralf.hertsch@phpmanufaktur.de>
+ * @link http://phpmanufaktur.de
+ * @copyright 2012
+ * @license MIT License (MIT) http://www.opensource.org/licenses/MIT
+ */
+
+if (!defined('LEPTON_PATH')) define('LEPTON_PATH', WB_PATH);
+if (!defined('LEPTON_URL')) define('LEPTON_URL', WB_URL);
