05d6a52a2e4174bca7616298930be71da78598aa
hertsch <ralf.hertsch@phpmanufaktur.de>
2012-08-08 05:29:45 +0200

    added Page Description, Title and Keywords

--- languages/DE.php
+++ languages/DE.php
@@ -61,12 +61,20 @@ $LANG = array(
       => 'Falls aktiviert, wird extendedWYSIWYG ein geschütztes Verzeichnis im /MEDIA Ordner anlegen und HTML Dateien für jeden Inhalt anlegen, der den Status BACKUP erhält. Die eingebundenen Bilder werden ebenfalls gesichert.',
     'max. Archives in Selection'
       => 'max. Archive in der Auswahl',
+    'Page Description'
+      => 'Beschreibung',
+    'Page Keywords'
+      => 'Schlüsselwörter',
+    'Page Title'
+      => 'Seitentitel',
     'Please help to improve Open Source Software and report this problem to the <a href="{{ url }}" target="_blank">phpManufaktur Addons Support</a> Group.'
       => 'Bitte helfen Sie mit Open Source Software zu verbessern und melden Sie dieses Problem der <a href="{{ url }}" target="_blank">phpManufaktur Addons Support</a> Gruppe.',
     'publish'
       => 'veröffentlichen',
     'Save'
       => 'Speichern',
+    'Show Page Settings'
+      => 'Seiteneinstellungen anzeigen',
     'The content of the section <b>{{ section_id }}</b> has not changed, so nothing was to save.'
       => 'Der Inhalt der Section <b>{{ section_id }}</b> wurde nicht verändert und deshalb auch nicht gespeichert.',
     'The maximum number of archives that will be shown in the selection list'
