be9ba2585eac4c8c88a900e3059ef63611236ac3
hertsch <ralf.hertsch@phpmanufaktur.de>
2012-11-26 12:10:19 +0100

    Updated precheck, install und upgrade for release 10.16

--- precheck.php
+++ precheck.php
@@ -32,20 +32,103 @@ else {
 // end include class.secure.php
 
 // Checking Requirements
+global $database;
 
-$PRECHECK['PHP_VERSION'] = array('VERSION' => '5.2.0', 'OPERATOR' => '>=');
-$PRECHECK['WB_ADDONS'] = array(
-    'dwoo' => array('VERSION' => '0.14', 'OPERATOR' => '>='),
-    'manufaktur_config' => array('VERSION' => '0.11', 'OPERATOR' => '>=')
+$checked = true;
+
+// check PHP version
+$PRECHECK['PHP_VERSION'] = array(
+    'VERSION' => '5.2.0',
+    'OPERATOR' => '>='
 );
 
-global $database;
+// modified precheck array
+$check = array(
+    'Dwoo' => array(
+        'directory' => 'dwoo',
+        'version' => '0.16',
+        'problem' => 'Dwoo => <b><a href="https://addons.phpmanufaktur.de/download.php?file=Dwoo" target="_blank">Download actual version</a></b>'
+    ),
+    'dropletsExtension' => array(
+        'directory' => 'droplets_extension',
+        'version' => '0.24',
+        'problem' => 'dropletsExtension => <b><a href="https://addons.phpmanufaktur.de/download.php?file=dropletsExtension" target="_blank">Download actual version</a></b>'
+    ),
+    'wbLib' => array(
+        'directory' => 'wblib',
+        'version' => '0.65',
+        'problem' => 'wbLib => <b><a href="https://github.com/webbird/wblib/downloads" target="_blank">Download actual version</a></b>'
+        ),
+    'LibraryAdmin' => array(
+        'directory' => 'libraryadmin',
+        'version' => '1.9',
+        'problem' => 'LibraryAdmin => <b><a href="http://jquery.lepton-cms.org/modules/download_gallery/dlc.php?file=75&id=1318585713" target="_blank">Download actual version</a></b>'
+         ),
+    'libJQuery' => array(
+        'directory' => 'lib_jquery',
+        'version' => '1.25',
+        'problem' => 'libJQuery => <b><a href="http://jquery.lepton-cms.org/modules/download_gallery/dlc.php?file=76&id=1320743410" target="_blank">Download actual version</a></b>'
+        ),
+    'manufakturConfig' => array(
+        'directory' => 'manufaktur_config',
+        'version' => '0.16',
+        'problem' => 'manufakturConfig => <b><a href="https://addons.phpmanufaktur.de/download.php?file=manufakturConfig" target="_blank">Download actual version</a></b>'
+        )
+);
+
+$versionSQL = "SELECT `version` FROM `".TABLE_PREFIX."addons` WHERE `directory`='%s'";
+
+foreach ($check as $name => $addon) {
+  // loop throug the addons and check the versions
+  $version = $database->get_one(sprintf($versionSQL, $addon['directory']), MYSQL_ASSOC);
+  if (false === ($status = version_compare(!empty($version) ? $version : '0', $addon['version'], '>='))) {
+    $checked = false;
+    $key = $addon['problem'];
+  }
+  else
+    $key = $name;
+  $PRECHECK['CUSTOM_CHECKS'][$key] = array(
+      'REQUIRED' => $addon['version'],
+      'ACTUAL' => !empty($version) ? $version : '- not installed -',
+      'STATUS' => $status
+  );
+}
+
+// check default charset
 $SQL = "SELECT `value` FROM `".TABLE_PREFIX."settings` WHERE `name`='default_charset'";
 $charset = $database->get_one($SQL, MYSQL_ASSOC);
-$PRECHECK['CUSTOM_CHECKS'] = array(
-	'Default Charset' => array(
-		'REQUIRED' => 'utf-8',
-		'ACTUAL' => $charset,
-		'STATUS' => ($charset === 'utf-8')
-	)
+if ($charset != 'utf-8') {
+  $checked = false;
+  $key = 'This addon needs UTF-8 as default charset!';
+}
+else
+  $key = 'UTF-8';
+
+$PRECHECK['CUSTOM_CHECKS'][$key] = array(
+    'REQUIRED' => 'utf-8',
+    'ACTUAL' => $charset,
+    'STATUS' => ($charset == 'utf-8')
 );
+
+// jQueryAdmin should be uninstalled
+if (file_exists(WB_PATH . '/modules/jqueryadmin/tool.php')) {
+  $checked = false;
+  $key = 'jQueryAdmin is <b>deprecated</b>, please uninstall and<br />use <b>LibraryAdmin</b> instead!';
+}
+else
+  $key = 'jQueryAdmin';
+
+$PRECHECK['CUSTOM_CHECKS'][$key] = array(
+    'REQUIRED' => 'REMOVED',
+    'ACTUAL' => (file_exists(WB_PATH . '/modules/jqueryadmin/tool.php')) ? 'INSTALLED' : 'REMOVED',
+    'STATUS' => (!file_exists(WB_PATH . '/modules/jqueryadmin/tool.php'))
+);
+
+if (!$checked) {
+  // if a problem occured prompt a hint and grant that the LEPTON/WB precheck fail
+  $PRECHECK['CUSTOM_CHECKS']['Please install or update all required addons.<br />Need help? Please contact the <b><a href="https://phpmanufaktur.de/support" target="_blank">phpManufaktur Support Group</a></b>.'] = array(
+      'REQUIRED' => 'OK',
+      'ACTUAL' => 'PROBLEM',
+      'STATUS' => false
+  );
+}
--- upgrade.php
+++ upgrade.php
@@ -143,6 +143,81 @@ if (!$database->query($SQL))
 @unlink(LEPTON_PATH.'/modules/wysiwyg/templates/backend/body.lte');
 @unlink(LEPTON_PATH.'/modules/wysiwyg/templates/backend/modify.lte');
 
+// install or upgrade droplets
+if (file_exists(WB_PATH.'/modules/droplets/functions.inc.php')) {
+  include_once(WB_PATH.'/modules/droplets/functions.inc.php');
+}
+
+if (!function_exists('wb_unpack_and_import')) {
+  function wb_unpack_and_import($temp_file, $temp_unzip) {
+    global $admin, $database;
+
+    // Include the PclZip class file
+    require_once (WB_PATH . '/include/pclzip/pclzip.lib.php');
+
+    $errors = array();
+    $count = 0;
+    $archive = new PclZip($temp_file);
+    $list = $archive->extract(PCLZIP_OPT_PATH, $temp_unzip);
+    // now, open all *.php files and search for the header;
+    // an exported droplet starts with "//:"
+    if (false !== ($dh = opendir($temp_unzip))) {
+      while (false !== ($file = readdir($dh))) {
+        if ($file != "." && $file != "..") {
+          if (preg_match('/^(.*)\.php$/i', $file, $name_match)) {
+            // Name of the Droplet = Filename
+            $name = $name_match[1];
+            // Slurp file contents
+            $lines = file($temp_unzip . '/' . $file);
+            // First line: Description
+            if (preg_match('#^//\:(.*)$#', $lines[0], $match)) {
+              $description = $match[1];
+            }
+            // Second line: Usage instructions
+            if (preg_match('#^//\:(.*)$#', $lines[1], $match)) {
+              $usage = addslashes($match[1]);
+            }
+            // Remaining: Droplet code
+            $code = implode('', array_slice($lines, 2));
+            // replace 'evil' chars in code
+            $tags = array(
+                '<?php',
+                '?>',
+                '<?'
+            );
+            $code = addslashes(str_replace($tags, '', $code));
+            // Already in the DB?
+            $stmt = 'INSERT';
+            $id = NULL;
+            $found = $database->get_one("SELECT * FROM " . TABLE_PREFIX . "mod_droplets WHERE name='$name'");
+            if ($found && $found > 0) {
+              $stmt = 'REPLACE';
+              $id = $found;
+            }
+            // execute
+            $result = $database->query("$stmt INTO " . TABLE_PREFIX . "mod_droplets VALUES('$id','$name','$code','$description','" . time() . "','" . $admin->get_user_id() . "',1,0,0,0,'$usage')");
+            if (!$database->is_error()) {
+              $count++;
+              $imports[$name] = 1;
+            }
+            else {
+              $errors[$name] = $database->get_error();
+            }
+          }
+        }
+      }
+      closedir($dh);
+    }
+    return array(
+        'count' => $count,
+        'errors' => $errors,
+        'imported' => $imports
+    );
+  } // function wb_unpack_and_import()
+}
+// install the droplet(s)
+@wb_unpack_and_import(WB_PATH.'/modules/wysiwyg/droplets/droplet_wysiwyg_teaser.zip', WB_PATH . '/temp/unzip/');
+
 require_once LEPTON_PATH.'/modules/manufaktur_config/library.php';
 
 // initialize the configuration
