4353998024c0096ecbc7044dbba368827bb87ad5
hertsch <ralf.hertsch@phpmanufaktur.de>
2013-01-17 17:52:31 +0100

    Added \phpManufaktur\Service

---
+++ vendor/phpManufaktur/Service/Service.php
@@ -0,0 +1,27 @@
+<?php
+
+/**
+ * extendedWYSIWYG
+ *
+ * @author Team phpManufaktur <team@phpmanufaktur.de>
+ * @link https://addons.phpmanufaktur.de/extendedWYSIWYG
+ * @copyright 2012 Ralf Hertsch <ralf.hertsch@phpmanufaktur.de>
+ * @license MIT License (MIT) http://www.opensource.org/licenses/MIT
+ */
+
+namespace phpManufaktur\Service;
+
+use phpManufaktur\CMS\Classes\boneClass;
+use phpManufaktur\CMS\Bridge;
+
+global $logger;
+global $tools;
+global $db;
+
+class Service extends boneClass {
+
+  public function exec() {
+    echo __METHOD__;
+  } // exec()
+
+} // class Service
