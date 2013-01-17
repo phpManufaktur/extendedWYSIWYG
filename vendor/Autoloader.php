77aa92630f806cedc3be86a5e8f85be55dfe9351
hertsch <ralf.hertsch@phpmanufaktur.de>
2013-01-17 17:45:49 +0100

    Added Autoloader

---
+++ vendor/Autoloader.php
@@ -0,0 +1,31 @@
+<?php
+
+/**
+ * extendedWYSIWYG
+ *
+ * @author Team phpManufaktur <team@phpmanufaktur.de>
+ * @link https://addons.phpmanufaktur.de/extendedWYSIWYG
+ * @copyright 2012 Ralf Hertsch <ralf.hertsch@phpmanufaktur.de>
+ * @license MIT License (MIT) http://www.opensource.org/licenses/MIT
+ */
+
+class Autoloader {
+
+  /**
+   * Autoloader function
+   *
+   * @param string $className
+   * @return boolean
+   */
+  static public function loader($className) {
+    $filename = __DIR__ . DIRECTORY_SEPARATOR . str_replace('\\', '/', $className) . '.php';
+    if (file_exists($filename)) {
+      include($filename);
+      if (class_exists($className)) return true;
+    }
+    return false;
+  }
+
+} // class Autoloader
+
+spl_autoload_register('Autoloader::loader');
