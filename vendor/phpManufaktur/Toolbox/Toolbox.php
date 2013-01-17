d1217e7c81e7510aef40c48577ad3f029ce0abb0
hertsch <ralf.hertsch@phpmanufaktur.de>
2013-01-17 17:52:06 +0100

    Added \vendor\phpManufaktur\Toolbox

---
+++ vendor/phpManufaktur/Toolbox/Toolbox.php
@@ -0,0 +1,58 @@
+<?php
+
+/**
+ * extendedWYSIWYG
+ *
+ * @author Team phpManufaktur <team@phpmanufaktur.de>
+ * @link https://addons.phpmanufaktur.de/extendedWYSIWYG
+ * @copyright 2012 Ralf Hertsch <ralf.hertsch@phpmanufaktur.de>
+ * @license MIT License (MIT) http://www.opensource.org/licenses/MIT
+ */
+
+namespace phpManufaktur\Toolbox;
+
+use phpManufaktur\CMS\Classes\boneClass;
+
+class Toolbox extends boneClass {
+
+  /**
+   * Sanitize variables and prepare them for saving in a MySQL record
+   *
+   * @param mixed $item
+   * @return mixed
+   */
+  public static function sanitizeVariable($item) {
+    if (!is_array($item)) {
+      // undoing 'magic_quotes_gpc = On' directive
+      if (get_magic_quotes_gpc())
+        $item = stripcslashes($item);
+      $item = self::sanitizeText($item);
+    }
+    return $item;
+  } // sanitizeVariable()
+
+  /**
+   * Sanitize a text variable and prepare ist for saving in a MySQL record
+   *
+   * @param string $text
+   * @return string
+   */
+  public static function sanitizeText($text) {
+    $text = str_replace(array("<",">","\"","'"), array("&lt;","&gt;","&quot;","&#039;"), $text);
+    $text = mysql_real_escape_string($text);
+    return $text;
+  } // sanitizeText()
+
+  /**
+   * Unsanitize a text variable and prepare it for output
+   *
+   * @param string $text
+   * @return string
+   */
+  public static function unsanitizeText($text) {
+    $text = stripcslashes($text);
+    $text = str_replace(array("&lt;","&gt;","&quot;","&#039;"), array("<",">","\"","'"), $text);
+    return $text;
+  } // unsanitizeText()
+
+} // class Toolbox
