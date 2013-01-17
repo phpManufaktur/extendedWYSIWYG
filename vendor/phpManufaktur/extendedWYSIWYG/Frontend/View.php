e1187feb73a1f4ff1b80df1f136bcf2521b91602
hertsch <ralf.hertsch@phpmanufaktur.de>
2013-01-17 17:50:37 +0100

    Added \vendor\phpManufaktur\extendedWYSIWYG

---
+++ vendor/phpManufaktur/extendedWYSIWYG/Frontend/View.php
@@ -0,0 +1,54 @@
+<?php
+
+/**
+ * extendedWYSIWYG
+ *
+ * @author Team phpManufaktur <team@phpmanufaktur.de>
+ * @link https://addons.phpmanufaktur.de/extendedWYSIWYG
+ * @copyright 2012 Ralf Hertsch <ralf.hertsch@phpmanufaktur.de>
+ * @license MIT License (MIT) http://www.opensource.org/licenses/MIT
+ */
+
+namespace phpManufaktur\extendedWYSIWYG\Frontend;
+
+use phpManufaktur\CMS\Classes\boneClass;
+use phpManufaktur\CMS\Bridge;
+
+global $logger;
+global $tools;
+
+class View extends boneClass {
+
+  /**
+   * Get the content for the $section_id and return it
+   *
+   * @return string
+   */
+  public function getSection($section_id) {
+    global $db;
+    global $tools;
+
+    $section_id = (int) $section_id;
+    if ($section_id < 1) {
+      // invalid section id!
+      $this->setError('The submitted Section ID is invalid', __METHOD__, __LINE__);
+      return $this->getError();
+    }
+    try {
+      $SQL = "SELECT content FROM `".CMS_TABLE_PREFIX."mod_wysiwyg` WHERE section_id=?";
+      $section = $db->fetchAssoc($SQL, array($section_id));
+    } catch (\Doctrine\DBAL\DBALException $e) {
+      $this->setError($e->getMessage(), __METHOD__, $e->getLine());
+      return $this->getError();
+    }
+
+    $section = $tools->unsanitizeText($section['content']);
+    if ((CMS_TYPE == 'WebsiteBaker') && (CMS_VERSION == '2.8.1')) {
+      // in WB 2.8.1 the [wblinks ...] must be preprocessed separate
+      global $wb;
+      $wb->preprocess($section);
+    }
+    return $section;
+  } // getSection()
+
+} // class View
