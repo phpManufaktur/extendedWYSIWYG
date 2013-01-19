8f07968b8f48f26bceb404c03016afce81bcb790
hertsch <ralf.hertsch@phpmanufaktur.de>
2013-01-19 12:46:15 +0100

    check if CKE is installed and active

---
+++ vendor/phpManufaktur/extendedWYSIWYG/Control/checkCKEditor.php
@@ -0,0 +1,44 @@
+<?php
+
+/**
+ * extendedWYSIWYG
+ *
+ * @author Team phpManufaktur <team@phpmanufaktur.de>
+ * @link https://addons.phpmanufaktur.de/extendedWYSIWYG
+ * @copyright 2012 Ralf Hertsch <ralf.hertsch@phpmanufaktur.de>
+ * @license MIT License (MIT) http://www.opensource.org/licenses/MIT
+ *
+ * This file will be called by jQuery placed at the section editing page.
+ */
+
+namespace phpManufaktur\extendedWYSIWYG\Control;
+
+use phpManufaktur\CMS\Bridge\Data\LEPTON as LEPTON;
+use phpManufaktur\CMS\Bridge\Data\WebsiteBaker as WebsiteBaker;
+use phpManufaktur\CMS\Bridge\Control\boneClass;
+
+class checkCKEditor extends boneClass {
+
+  /**
+   * Check if the CKEditor is installed
+   *
+   * @return boolean
+   */
+  public function isInstalled() {
+    return file_exists(CMS_PATH.'/modules/ckeditor/info.php');
+  } // isInstalled()
+
+  /**
+   * Check if the CKEditor is the active editor for the CMS
+   *
+   * @return boolean
+   */
+  public function isActiveEditor() {
+    if (CMS_TYPE == 'LEPTON')
+      $setting = new LEPTON\Setting();
+    else
+      $setting = new WebsiteBaker\Setting();
+    return ($setting->get('ckeditor') == 'ckeditor');
+  } // isActiveEditor()
+
+} // class checkCKEactive
