459c2c0a8a64f3ff40787cf15171d29090f5c55f
hertsch <ralf.hertsch@phpmanufaktur.de>
2013-01-19 12:45:51 +0100

    Added page settings class

---
+++ vendor/phpManufaktur/CMS/Bridge/Data/LEPTON/Setting.php
@@ -0,0 +1,40 @@
+<?php
+
+/**
+ * extendedWYSIWYG
+ *
+ * @author Team phpManufaktur <team@phpmanufaktur.de>
+ * @link https://addons.phpmanufaktur.de/extendedWYSIWYG
+ * @copyright 2012 Ralf Hertsch <ralf.hertsch@phpmanufaktur.de>
+ * @license MIT License (MIT) http://www.opensource.org/licenses/MIT
+ */
+
+namespace phpManufaktur\CMS\Bridge\Data\LEPTON;
+
+use phpManufaktur\CMS\Bridge\Control\boneClass;
+
+global $db;
+
+class Setting extends boneClass {
+
+  /**
+   * Get the value for given setting from the LEPTON configuration
+   *
+   * @param string $setting
+   * @return boolean|Ambigous <>
+   */
+  public function get($setting) {
+    global $db;
+
+    try {
+      $SQL = "SELECT `value` FROM `".CMS_TABLE_PREFIX."settings` WHERE `name`= ?";
+      $this->setInfo("SQL: $SQL", __METHOD__, __LINE__);
+      $result = $db->fetchAssoc($SQL, array($setting));
+    } catch (\Doctrine\DBAL\DBALException $e) {
+      $this->setError($e->getMessage(), __METHOD__, $e->getLine());
+      return false;
+    }
+    return $result['value'];
+  } // get()
+
+}
---
+++ vendor/phpManufaktur/CMS/Bridge/Data/WebsiteBaker/Setting.php
@@ -0,0 +1,18 @@
+<?php
+
+/**
+ * extendedWYSIWYG
+ *
+ * @author Team phpManufaktur <team@phpmanufaktur.de>
+ * @link https://addons.phpmanufaktur.de/extendedWYSIWYG
+ * @copyright 2012 Ralf Hertsch <ralf.hertsch@phpmanufaktur.de>
+ * @license MIT License (MIT) http://www.opensource.org/licenses/MIT
+ */
+
+namespace phpManufaktur\CMS\Bridge\Data\WebsiteBaker;
+
+use phpManufaktur\CMS\Bridge\Data\LEPTON as LEPTON;
+
+global $db;
+
+class Setting extends LEPTON\Setting { }
