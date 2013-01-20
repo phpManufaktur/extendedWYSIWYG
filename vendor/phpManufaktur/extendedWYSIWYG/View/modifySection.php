c331022d58dbaae0bb3aadf611643010b54b6de7
hertsch <ralf.hertsch@phpmanufaktur.de>
2013-01-20 16:39:19 +0100

    Teh modify dialog ist just in progress

--- vendor/phpManufaktur/extendedWYSIWYG/View/modifySection.php
+++ vendor/phpManufaktur/extendedWYSIWYG/View/modifySection.php
@@ -17,6 +17,8 @@ use phpManufaktur\extendedWYSIWYG\Data\wysiwygSection;
 use phpManufaktur\extendedWYSIWYG\Data\wysiwygOptions;
 use phpManufaktur\extendedWYSIWYG\Data\pageSettings;
 use phpManufaktur\extendedWYSIWYG\Data\wysiwygArchive;
+use phpManufaktur\extendedWYSIWYG\Data\wysiwygTeaser;
+use phpManufaktur\CMS\Bridge\Data\LEPTON as LEPTON;
 
 require_once CMS_PATH.'/modules/dwoo/dwoo-1.1.1/dwoo/Dwoo/Exception.php';
 
@@ -25,15 +27,19 @@ class modifySection extends boneClass {
   const ANCHOR = 'wysiwyg_';
 
   const REQUEST_ACTION = 'act';
+  const REQUEST_TEASER_CONTENT = 'teaser_text';
+  const REQUEST_TEASER_ID = 'teaser_id';
 
   const ACTION_MODIFY = 'mod';
 
   protected static $PAGE_ID = null;
   protected static $SECTION_ANCHOR = null;
   protected static $SECTION_ID = null;
+  protected static $SECTION_POSITION = null;
   protected static $TEMPLATE_PATH = null;
   protected static $ARCHIVE_ID = null;
   protected static $MODIFY_PAGE_URL = null;
+  protected static $TEASER_ID = null;
 
 
   protected $lang = null;
@@ -69,6 +75,24 @@ class modifySection extends boneClass {
   } // getArchiveID()
 
   /**
+   * Set the TEASER ID
+   *
+   * @param integer $teaser_id
+   */
+  public function setTeaserID($teaser_id) {
+    self::$TEASER_ID = $teaser_id;
+  } // setTeaserID()
+
+  /**
+   * Get the active TEASER ID
+   *
+   * @return integer|NULL
+   */
+  public function getTeaserID() {
+    return self::$TEASER_ID;
+  } // getTeaserID()
+
+  /**
    * Get the template, set the data and return the compiled
    *
    * @param string $template the name of the template
@@ -101,6 +125,7 @@ class modifySection extends boneClass {
     $content = $this->prepareDialog();
 
     $data = array(
+        'section_position' => self::$SECTION_POSITION,
         'CMS_ADDON_URL' => CMS_ADDON_URL,
         'anchor' => self::$SECTION_ANCHOR,
         'is_error' => (int) $this->isError(),
@@ -109,13 +134,18 @@ class modifySection extends boneClass {
     return $this->getTemplate('body.dwoo', $data);
   }
 
+  /**
+   * Prepare the WYSIWYG dialog, collect all data and return the filled template
+   *
+   * @return string WYSIWYG dialog
+   */
   protected function prepareDialog() {
     global $tools;
     global $cms;
 
     // get the position of the section
     $section = new wysiwygSection();
-    $position = $section->getSectionPositionInPage(self::$SECTION_ID);
+    self::$SECTION_POSITION = $section->getSectionPositionInPage(self::$SECTION_ID);
 
     // get the options of the section
     $opt = new wysiwygOptions();
@@ -165,12 +195,13 @@ class modifySection extends boneClass {
       self::$ARCHIVE_ID = $archive_id;
       $publish = true;
     }
-    $archives = $archive->selectArchiveListForDialog(self::$SECTION_ID);
-    if ($archive->isError()) {
+
+    // get the list of the last archives for the selection
+    if (false === ($archives = $archive->selectArchiveListForDialog(self::$SECTION_ID))) {
       $this->setError($archive->getError(), __METHOD__, __LINE__);
       return false;
     }
-
+    // bild the <select> array
     $archive_array = array();
     foreach ($archives as $item) {
       $archive_array[$item['archive_id']] = array(
@@ -179,7 +210,45 @@ class modifySection extends boneClass {
           );
     }
 
-    $leptoken = (defined('LEPTON_VERSION') && isset($_GET['leptoken'])) ? sprintf('&leptoken=%s', $_GET['leptoken']) : '';
+    // initialize Teaser
+    $teaser = new wysiwygTeaser();
+
+    // get the list of the last teasers for the selection
+    if (false === ($teasers = $teaser->selectTeaserListForDialog(self::$PAGE_ID))) {
+      $this->setError($teaser->getError(), __METHOD__, __LINE__);
+      return false;
+    }
+    // bild the <select> array
+    $teaser_archive = array();
+    foreach ($teasers as $item) {
+      $teaser_archive[$item['teaser_id']] = array(
+          'text' => sprintf('%s | %s', $item['timestamp'], $item['status']),
+          'value' => $item['teaser_id']
+      );
+    }
+
+    // get the teaser content for this page
+    if (!is_null(self::$TEASER_ID) && (self::$TEASER_ID > 0)) {
+      // get a specific Teaser content
+      if (false === ($teaser_record = $teaser->select(self::$TEASER_ID))) {
+        $this->setError($teaser->getError(), __METHOD__, __LINE__);
+        return false;
+      }
+      $teaser_text = $teaser_record['teaser_text'];
+      $teaser_id = self::$TEASER_ID;
+      $teaser_publish = (int) ($teaser_record['status'] == 'ACTIVE');
+    }
+    else {
+      // get the last Teaser content
+      if (false === ($teaser_record = $teaser->selectLast(self::$PAGE_ID))) {
+        $this->setError($teaser->getError(), __METHOD__, __LINE__);
+        return false;
+      }
+      // set the values from the record or default values
+      $teaser_text = isset($teaser_record['teaser_text']) ? $teaser_record['teaser_text'] : '';
+      $teaser_id = isset($teaser_record['teaser_id']) ? $teaser_record['teaser_id'] : -1;
+      $teaser_publish = isset($teaser_record['status']) ? (int) ($teaser_record['status'] == 'ACTIVE') : 1;
+    }
 
     $data = array(
         'page' => array(
@@ -190,7 +259,8 @@ class modifySection extends boneClass {
             'name' => 'section_id',
             'id' => self::$SECTION_ID,
             'editor_id' => 'content_'.self::$SECTION_ID,
-            'text' => $section_content
+            'text' => $section_content,
+            'position' => self::$SECTION_POSITION
             ),
         'publish' => $publish,
         'author' => $author,
@@ -207,14 +277,14 @@ class modifySection extends boneClass {
                 http_build_query(array(
                     'page_id' => self::$PAGE_ID,
                     )),
-                $leptoken,
+                LEPTON\Leptoken::getParameterString('&'), // use LEPTOKEN if needed!
                 self::$SECTION_ID
                 ),
             'anchor' => self::$SECTION_ANCHOR
             ),
         'options' => array(
             'page_settings' => array(
-                'active' => ($position == 1) ? 1 : 0,
+                'active' => (self::$SECTION_POSITION == 1) ? 1 : 0,
                 'checkbox' => array(
                     'name' => sprintf('page_settings_%d', self::$SECTION_ID),
                     'value' => 1,
@@ -242,7 +312,36 @@ class modifySection extends boneClass {
                     'checked' => in_array('hide_section', $options) ? 1 : 0
                     )
                 ),
-
+            'use_as_blog' => array(
+                'active' => (self::$SECTION_POSITION == 1) ? 1 : 0,
+                'checkbox' => array(
+                    'name' => sprintf('use_as_blog_%d', self::$SECTION_ID),
+                    'value' => 1,
+                    'checked' => in_array('use_as_blog', $options) ? 1 : 0
+                    ),
+                'fields' => array(
+                    'teaser' => array(
+                        'id' => $teaser_id,
+                        'name' => self::REQUEST_TEASER_CONTENT,
+                        'value' => $teaser_text,
+                        'publish' => $teaser_publish
+                        ),
+                    'archive' => array(
+                        'active' => empty($teaser_archive) ? 0 : 1,
+                        'name' => self::REQUEST_TEASER_ID,
+                        'items' => $teaser_archive,
+                        'link' => sprintf('%s?%s%s&%s=',
+                            self::$MODIFY_PAGE_URL,
+                            http_build_query(array(
+                                'page_id' => self::$PAGE_ID
+                            )),
+                            LEPTON\Leptoken::getParameterString('&'), // use LEPTOKEN if needed
+                            self::REQUEST_TEASER_ID
+                            ),
+                        'anchor' => self::$SECTION_ANCHOR
+                        )
+                    ),
+                )
             ),
         'link' => array(
             'save' => array(
