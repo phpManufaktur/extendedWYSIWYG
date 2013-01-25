1410fe2ffa1e763ea9acee0d5767de69022294fa
hertsch <ralf.hertsch@phpmanufaktur.de>
2013-01-25 06:46:55 +0100

    kitCommands are just in progress

---
+++ vendor/phpManufaktur/kitCommand/Command/wysiwyg/wysiwyg.php
@@ -0,0 +1,80 @@
+<?php
+
+/**
+ * extendedWYSIWYG
+ *
+ * @author Team phpManufaktur <team@phpmanufaktur.de>
+ * @link https://addons.phpmanufaktur.de/extendedWYSIWYG
+ * @copyright 2012 Ralf Hertsch <ralf.hertsch@phpmanufaktur.de>
+ * @license MIT License (MIT) http://www.opensource.org/licenses/MIT
+ */
+
+
+class wysiwyg {
+
+  /**
+   * Search and replace function
+   *
+   * @param string $content
+   * @param string $command_expression
+   * @param string $value
+   * @return mixed
+   */
+  protected function replace($content, $command_expression, $value) {
+    if (strtoupper($value) == 'CMS_MEDIA_URL') {
+      // replace the placeholder with real URL of the media directory
+      $content = str_replace($command_expression, CMS_MEDIA_URL, $content);
+      return $content;
+    }
+    if (false !== strpos($value, ',')) {
+      // search and replace the given terms
+      $search_pair = explode(',', $value);
+      if (count($search_pair) == 2) {
+        $search = trim($search_pair[0]);
+        $replace = trim($search_pair[1]);
+        // remove the $command_expression
+        $content = str_replace($command_expression, '', $content);
+        // search and replace the pair
+        $content = str_replace($search, $replace, $content);
+        return $content;
+      }
+    }
+    return $content;
+  } // replace()
+
+  protected function striptags($content, $command_expression, $value='') {
+    // remove the $command_expression
+    $content = str_replace($command_expression, '', $content);
+    $content = trim(strip_tags($content));
+    return $content;
+  } // striptags()
+
+  /**
+   * Execute the kitCommand "wysiwyg"
+   *
+   * @param string $content
+   * @param string $command_expression
+   * @param string $command
+   * @param string $params
+   * @param array &$special
+   * @return string
+   */
+  public function exec($content, $command_expression, $command, $params) {
+
+    foreach ($params as $parameter => $value) {
+      $parameter = strtolower($parameter);
+      switch ($parameter):
+      case 'replace':
+        $content = $this->replace($content, $command_expression, $value);
+        break;
+      case 'striptags':
+        $content = $this->striptags($content, $command_expression, $value);
+        break;
+      endswitch;
+    }
+
+    return $content;
+  } // exec()
+
+} // class wysiwyg
+
