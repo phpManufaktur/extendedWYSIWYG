0339373eff9cfb53debcaa794bc3bbd1d6a6a43e
hertsch <ralf.hertsch@phpmanufaktur.de>
2013-01-25 06:47:13 +0100

    added install and upgrade classes

---
+++ vendor/phpManufaktur/extendedWYSIWYG/Data/Setup/install.php
@@ -0,0 +1,205 @@
+<?php
+
+/**
+ * extendedWYSIWYG
+ *
+ * @author Team phpManufaktur <team@phpmanufaktur.de>
+ * @link https://addons.phpmanufaktur.de/extendedWYSIWYG
+ * @copyright 2012 Ralf Hertsch <ralf.hertsch@phpmanufaktur.de>
+ * @license MIT License (MIT) http://www.opensource.org/licenses/MIT
+ */
+
+namespace phpManufaktur\extendedWYSIWYG\Data\Setup;
+
+use phpManufaktur\extendedWYSIWYG\Data\wysiwygConfiguration;
+
+use phpManufaktur\extendedWYSIWYG\Data\wysiwygTeaser;
+
+use phpManufaktur\extendedWYSIWYG\Data\wysiwygExtension;
+use phpManufaktur\extendedWYSIWYG\Data\wysiwygArchive;
+use phpManufaktur\CMS\Bridge\Control\boneClass;
+use phpManufaktur\extendedWYSIWYG\Data\wysiwygSection;
+
+class install extends boneClass {
+
+  /**
+   * Change the regular addon name from 'wysiwyg' to 'extendedWYSIWYG'
+   *
+   * @return boolean
+   */
+  protected function changeAddonName() {
+    global $db;
+
+    try {
+      $db->update(CMS_TABLE_PREFIX.'addons', array('name' => 'extendedWYSIWYG'),
+          array('directory' => 'wysiwyg'));
+    } catch (\Doctrine\DBAL\DBALException $e) {
+      $this->setError($e->getMessage(), __METHOD__, $e->getLine());
+      return false;
+    }
+    return true;
+  } // changeAddonName()
+
+  protected function createTables() {
+    // create table mod_wysiwyg
+    $wysiwygSection = new wysiwygSection();
+    if (!$wysiwygSection->create()) {
+      $this->setError($wysiwygSection->getError(), __METHOD__, __LINE__);
+      return false;
+    }
+
+    // create table mod_wysiwyg_archive
+    $wysiwygArchive = new wysiwygArchive();
+    if (!$wysiwygArchive->create()) {
+      $this->setError($wysiwygArchive->getError(), __METHOD__, __LINE__);
+      return false;
+    }
+
+    // create table mod_wysiwyg_extension
+    $wysiwygExtension = new wysiwygExtension();
+    if (!$wysiwygExtension->create()) {
+      $this->setError($wysiwygExtension->getError(), __METHOD__, __LINE__);
+      return false;
+    }
+
+    // create table mod_wysiwyg_teaser
+    $wysiwygTeaser = new wysiwygTeaser();
+    if (!$wysiwygTeaser->create()) {
+      $this->setError($wysiwygTeaser->getError(), __METHOD__, __LINE__);
+      return false;
+    }
+
+    // initialize the configuration for extendedWYSIWYG
+    $wysiwygConfiguration = new wysiwygConfiguration();
+    $path = CMS_ADDON_CONFIG_PATH.'/extendedWYSIWYG.xml';
+    if (!$wysiwygConfiguration->readXMLfile($path, 'wysiwyg', true)) {
+      $this->setError($wysiwygConfiguration->getError(), __METHOD__, __LINE__);
+      return false;
+    }
+
+    return true;
+  } // createTables()
+
+  /**
+   * Check if the WebsiteBaker output filter is already patched
+   *
+   * @param string $filter_path
+   * @return boolean
+   */
+  protected function websiteBakerIsPatched($filter_path) {
+    if (file_exists($filter_path)) {
+      $lines = file($filter_path);
+      foreach ($lines as $line)
+        if (strpos($line, "extendedWYSIWYG") > 0) return true;
+    }
+    return false;
+  } // websiteBakerIsPatched()
+
+  /**
+   * Patch the WebsiteBaker output filter
+   *
+   * @param string $filter_path
+   * @param boolean $wb_283
+   * @return boolean
+   */
+  protected function websiteBakerDoPatch($filter_path, $wb_283=false) {
+    $search = $wb_283 ? "define('OUTPUT_FILTER_DOT_REPLACEMENT'" : 'function filter_frontend_output($content)';
+    $returnvalue = false;
+    $tempfile = CMS_PATH .'/modules/output_filter/new_filter.php';
+    $backup = CMS_PATH .'/modules/output_filter/original-extended-wysiwyg-filter-routines.php';
+
+    $addline = "\n\n\t\t// exec extendedWYSIWYG filter";
+    $addline .= "\n\t\tif(file_exists(WB_PATH .'/modules/wysiwyg/vendor/phpManufaktur/CMS/Bridge/Control/outputFilter.php')) { ";
+    $addline .= "\n\t\t\trequire_once (WB_PATH .'/modules/wysiwyg/vendor/phpManufaktur/CMS/Bridge/Control/outputFilter.php'); ";
+    $addline .= "\n\t\t\t".'$content = cmsBridgeFilter($content); ';
+    $addline .= "\n\t\t}\n\n ";
+
+    if (file_exists($filter_path)) {
+      $lines = file ($filter_path);
+      $handle = fopen ($tempfile, 'w');
+      foreach ($lines as $line) {
+        fwrite ($handle, $line);
+        if (strpos($line, $search) > 0) {
+          $returnvalue = true;
+          fwrite($handle, $addline);
+        }
+      }
+      fclose ($handle);
+      if (rename($filter_path, $backup)) {
+        if (rename($tempfile, $filter_path)) {
+          return $returnvalue;
+        }
+        else {
+          return false;
+        }
+      }
+    }
+    return false;
+  }
+
+  /**
+   * Add the output filter for extendedWYSIWYG
+   *
+   * @return boolean
+   */
+  protected function addFilter() {
+    if (CMS_TYPE == 'LEPTON') {
+      // register the filter at LEPTON outputInterface
+      if (!file_exists(CMS_PATH .'/modules/output_interface/output_interface.php')) {
+        $this->setError('Missing LEPTON outputInterface, can\'t register the cmsBridge filter - installation is not complete!',
+            __METHOD__, __LINE__);
+        return false;
+      }
+      else {
+        if (!function_exists('register_output_filter'))
+          include_once(CMS_PATH .'/modules/output_interface/output_interface.php');
+        register_output_filter('wysiwyg', 'extendedWYSIWYG');
+      }
+    }
+    else {
+      if (version_compare(CMS_VERSION, '2.8.3', '>=')) {
+        // WebsiteBaker 2.8.3
+        $wb_283 = true;
+        $filter_path = CMS_PATH.'/modules/output_filter/index.php';
+      }
+      else {
+        // all other WebsiteBaker versions
+        $wb_283 = false;
+        $filter_path = CMS_PATH .'/modules/output_filter/filter-routines.php';
+      }
+      if (file_exists($filter_path)) {
+        if (!$this->websiteBakerIsPatched($filter_path)) {
+          if (!$this->websiteBakerDoPatch($filter_path, $wb_283)) {
+            $this->setError('Failed to patch the WebsiteBaker output filter, please contact the support!', __METHOD__, __LINE__);
+            return false;
+          }
+        }
+      }
+      else {
+        $this->setError('Can\'t detect the correct method to patch the output filter, please contact the support!', __METHOD__, __LINE__);
+        return false;
+      }
+    }
+    return true;
+  } // addFilter()
+
+  /**
+   * Execute the installation
+   *
+   * @return boolean
+   */
+  public function exec() {
+    // change the addon name
+    if (!$this->changeAddonName())
+      return false;
+    // create the tables
+    if (!$this->createTables())
+      return false;
+    // add the output filter
+    if (!$this->addFilter())
+      return false;
+    // install success!
+    return true;
+  } // exec()
+
+} // class install
---
+++ vendor/phpManufaktur/extendedWYSIWYG/Data/Setup/upgrade.php
@@ -0,0 +1,94 @@
+<?php
+
+/**
+ * extendedWYSIWYG
+ *
+ * @author Team phpManufaktur <team@phpmanufaktur.de>
+ * @link https://addons.phpmanufaktur.de/extendedWYSIWYG
+ * @copyright 2012 Ralf Hertsch <ralf.hertsch@phpmanufaktur.de>
+ * @license MIT License (MIT) http://www.opensource.org/licenses/MIT
+ */
+
+namespace phpManufaktur\extendedWYSIWYG\Data\Setup;
+
+class upgrade extends install {
+
+  /**
+   * Process the upgrade especially for Release 11.01
+   *
+   * @return boolean
+   */
+  protected function release_1101() {
+    global $tools;
+    global $db;
+
+    // some directories are no longer needed
+    $delete_directories = array(
+        '/languages',
+        '/classes',
+        '/templates',
+        '/droplets',
+        '/presets',
+        '/templates',
+        '/images',
+        '/restore'
+        );
+    foreach ($delete_directories as $directory) {
+      if (!$tools->deleteDirectory(CMS_ADDON_PATH.$directory))
+        return false;
+    }
+
+    // delete no longer needed files
+    $delete_files = array(
+        '/save.php',
+        '/backend.js',
+        '/class.wysiwyg.php',
+        '/wb2lepton.php',
+        '/wysiwyg_teaser.css',
+        '/custom.wysiwyg_teaser.css'
+        );
+    foreach ($delete_files as $file)
+      @unlink(CMS_ADDON_PATH.$file);
+
+    // the droplet wysiwyg_teaser is no longer supported
+    try {
+      $db->delete(CMS_TABLE_PREFIX.'mod_droplets', array('name' => 'wysiwyg_teaser'));
+    } catch (\Doctrine\DBAL\DBALException $e) {
+      $this->setError($e->getMessage(), __METHOD__, $e->getLine());
+      return false;
+    }
+
+    return true;
+  } // release_1101()
+
+  /**
+   * Exec the upgrade process for extendedWYSIWYG
+   *
+   * @see \phpManufaktur\extendedWYSIWYG\Data\Setup\install::exec()
+   */
+  public function exec() {
+    // GIT files can be deleted
+    @unlink(CMS_ADDON_PATH.'/.gitignore');
+    @unlink(CMS_ADDON_PATH.'/gitattributes');
+
+    // change the addon name
+    if (!$this->changeAddonName())
+      return false;
+
+    // check if all tables are created
+    if (!$this->createTables())
+      return false;
+
+    // add the output filter
+    if (!$this->addFilter())
+      return false;
+
+    // Release 11.01
+    if (!$this->release_1101())
+      return false;
+
+    // upgrade successfull
+    return true;
+  } // exec()
+
+} // class install
