c3a5c290eac4dd18b211b4ea0f07984518447db4
hertsch <ralf.hertsch@phpmanufaktur.de>
2013-01-27 16:08:44 +0100

    just in progress ...

--- vendor/phpManufaktur/extendedWYSIWYG/Data/Location/DE.php
+++ vendor/phpManufaktur/extendedWYSIWYG/Data/Location/DE.php
@@ -17,10 +17,14 @@ if ('á' != "\xc3\xa1") {
 $LANG = array(
     '- please select -'
       => '- bitte auswählen -',
+    '- Unique root page for all editors -'
+      => 'Universelle Stammseite für alle Redakteure',
     'A change of a WYSIWYG section will update the last modified field of the page (recommend!)'
       => 'Die Änderung eines WYSIWYG Abschnitt löst die Aktualisierung des "zuletzt geändert" Feldes der Seite aus (empfohlen!)',
     'Actual Message'
       => 'Aktuelle Meldung',
+    'Add department'
+      => 'Ressort hinzufügen',
     'Add editor'
       => 'Redakteur hinzufügen',
     '<p>At least you must specify one department for your editorial team.</p>'
@@ -28,16 +32,32 @@ $LANG = array(
 
     'Change'
       => 'Ändern',
+    'Change department'
+      => 'Ressort ändern',
     'Chars'
       => 'Zeichen',
+    'CHIEF_EDITOR'
+      => 'Chefredakteur',
     'Create Archive Files'
       => 'Archivdateien anlegen',
 
+    'Department description'
+      => 'Ressort Beschreibung',
+    'Department list'
+      => 'Ressort Liste',
+    'Department name'
+      => 'Ressort Bezeichnung',
     'Departments'
       => 'Ressorts',
 
+    'Edit department'
+      => 'Ressort bearbeiten',
     'Editor'
       => 'Redakteur',
+    'EDITOR'
+      => 'Redakteur',
+    'Editor list'
+      => 'Liste der Redakteure',
     'Editorial department'
       => 'Redaktion',
     'Editorial team'
@@ -65,6 +85,8 @@ $LANG = array(
 
     'General'
       => 'Allgemein',
+    'Got a invalid ID for the editor!'
+      => 'Ungültige ID für das Bearbeiten eines Redakteurs!',
 
     'Hide Section'
       => 'Abschnitt einklappen',
@@ -94,8 +116,16 @@ $LANG = array(
       => 'Seitentitel',
     'Password'
       => 'Passwort',
+    '<p>Please determine at least one department for your editorial team.</p><p>In the case you want to edit all pages of your website with the same team, set the root page below to "Unique root for all editors".</p><p>In all other cases select a page as root page for the department. All pages below this one will be accessible for the edtiors belonging to this department.</p><p>Departments can\'t be nested!</p>'
+      => '<p>Legen Sie mindestens ein Ressort für Ihr Redaktionsteam fest.</p></p>Für den Fall, dass Sie alle Seiten Ihrer Website mit dem gleichen Team bearbeiten und organisieren möchten, setzen Sie die Stammseite auf "Universelle Stammseite für alle Redakteure".</p><p>In allen anderen Fällen wählen Sie eine beliebige Seite als Stammseite für das Ressort aus.</p><p>Ressorts können nicht ineinander verschachtelt werden!</p>',
+    '<p>Please edit the department with the ID {{ id }}.</p>'
+      => 'Bearbeiten Sie das Ressort mit der ID {{ id }}.</p>',
+    '<p>Please edit the editor with the ID {{ id }}.</p>'
+      => '<p>Bearbeiten Sie die Angaben zu dem Redakteur mit der ID {{ id }}.</p>',
     'Please help to improve Open Source Software and report this problem to the <a href="{{ url }}" target="_blank">phpManufaktur Addons Support</a> Group.'
       => 'Bitte helfen Sie mit Open Source Software zu verbessern und melden Sie dieses Problem der <a href="{{ url }}" target="_blank">phpManufaktur Addons Support</a> Gruppe.',
+    '<p>Please select a root page and a name for the new department!</p>'
+      => '<p>Bitte wählen Sie eine Stammseite auns und geben Sie dem neuen Ressort einen Namen!</p>',
     '<p>Please type in your username and password!</p>'
       => '<p>Bitte geben Sie Ihren Benutzernamen und Ihr Passwort an!</p>',
     'publish'
@@ -106,12 +136,16 @@ $LANG = array(
 
     'Save'
       => 'Speichern',
+    'Select root page'
+      => 'Stammseite auswählen',
     'Select user'
-      => 'Benutzer auswählen',
+      => 'CMS Benutzer auswählen',
     'Show Page Settings'
       => 'Seiteneinstellungen anzeigen',
     'Specify if you want to use the Editorial department system of extendedWYSIWYG or not. Please read the documentation before you activate the Editorial department!'
       => 'Legen Sie fest, ob Sie das Redaktionssystem von extendedWYSIWYG verwenden möchten oder nicht. Bitte lesen Sie unbedingt die Dokumentation bevor Sie das System aktivieren!',
+    'SUB_CHIEF_EDITOR'
+      => 'stv. Chefredakteur',
 
     '<p>The content of the section <b>{{ section_id }}</b> has not changed, so nothing was to save.</p>'
       => '<p>Der Inhalt der Section <b>{{ section_id }}</b> wurde nicht verändert und deshalb auch nicht gespeichert.</p>',
@@ -129,6 +163,8 @@ $LANG = array(
       => '<p>Die <b>aktuelle Meldung</b> für die Seite wurde aktualisiert.</p>',
     '<p>The section <b>{{ section_id }}</b> was successfull saved.</p>'
       => '<p>Die Section <b>{{ section_id }}</b> wurde erfolgreich gespeichert.</p>',
+    'TRAINEE'
+      => 'Volontär',
 
     'Update page information'
       => 'Seiten aktualisieren',
