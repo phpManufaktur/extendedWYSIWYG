ae7aeffea3ebbb17383d73a192362ffd2f0292bf
hertsch <ralf.hertsch@phpmanufaktur.de>
2013-01-29 20:48:12 +0100

    added selectMenuTitle(), selectPageTitle()
    and delete()

--- vendor/phpManufaktur/CMS/Bridge/Data/LEPTON/Pages.php
+++ vendor/phpManufaktur/CMS/Bridge/Data/LEPTON/Pages.php
@@ -63,4 +63,61 @@ class Pages extends boneClass {
     return true;
   } // fixRootLevelProblem()
 
+  /**
+   * Select the menu title for the given page id
+   *
+   * @param integer $page_id
+   * @return boolean|Ambigous <string, mixed>
+   */
+  public function selectMenuTitle($page_id) {
+    global $db;
+    global $tools;
+
+    try {
+      $SQL = "SELECT `menu_title` FROM `".CMS_TABLE_PREFIX."pages` WHERE `page_id`='$page_id'";
+      $result = $db->fetchAssoc($SQL);
+    } catch (\Doctrine\DBAL\DBALException $e) {
+      $this->setError($e->getMessage(), __METHOD__, $e->getLine());
+      return false;
+    }
+    return (isset($result['menu_title'])) ? $tools->unsanitizeText($result['menu_title']) : false;
+  } // selectMenuTitle()
+
+  /**
+   * Select the page title for the given page id
+   *
+   * @param integer $page_id
+   * @return boolean|Ambigous <string, mixed>
+   */
+  public function selectPageTitle($page_id) {
+    global $db;
+    global $tools;
+
+    try {
+      $SQL = "SELECT `page_title` FROM `".CMS_TABLE_PREFIX."pages` WHERE `page_id`='$page_id'";
+      $result = $db->fetchAssoc($SQL);
+    } catch (\Doctrine\DBAL\DBALException $e) {
+      $this->setError($e->getMessage(), __METHOD__, $e->getLine());
+      return false;
+    }
+    return (isset($result['page_title'])) ? $tools->unsanitizeText($result['page_title']) : false;
+  } // selectPageTitle()
+
+  /**
+   * Delete the page with the given PAGE_ID
+   *
+   * @param integer $page_id
+   * @return boolean
+   */
+  public function delete($page_id) {
+    global $db;
+
+    try {
+      $db->delete(CMS_TABLE_PREFIX.'pages', array('page_id' => $page_id));
+    } catch (\Doctrine\DBAL\DBALException $e) {
+      $this->setError($e->getMessage(), __METHOD__, $e->getLine());
+      return false;
+    }
+    return true;
+  }
 } // class mediaDirectory
