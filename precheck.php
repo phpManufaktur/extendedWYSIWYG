bac66e091014be7c2c8faab64daa6a3f71185ce9
hertsch <ralf.hertsch@phpmanufaktur.de>
2013-02-07 09:52:41 +0100

    improved precheck to prevent problems

--- precheck.php
+++ precheck.php
@@ -17,9 +17,10 @@ global $database;
 $checked = true;
 
 // check PHP version
-$PRECHECK['PHP_VERSION'] = array(
-    'VERSION' => '5.3.2',
-    'OPERATOR' => '>='
+$PRECHECK['CUSTOM_CHECKS']['PHP Version'] = array(
+    'REQUIRED' => '5.3.2',
+    'ACTUAL' => PHP_VERSION,
+    'STATUS' => versionCompare(PHP_VERSION, '5.3.2', '>=')
 );
 
 // modified precheck array
@@ -53,6 +54,58 @@ foreach ($check as $name => $addon) {
       'STATUS' => $status
   );
 }
+
+// check WebsiteBaker / LEPTON
+$wb_version = $database->get_one("SELECT `value` FROM `".TABLE_PREFIX."settings` WHERE `name`='wb_version'");
+if (!empty($wb_version)) {
+  // WebsiteBaker
+  if (!in_array($wb_version, array('2.8.2', '2.8.3'))) {
+    $checked = false;
+    $key = "The WebsiteBaker version $wb_version is not approved for extendedWYSIYG!";
+    $status = false;
+  }
+  else {
+    $key = 'CMS Version';
+    $wb_version = 'CMS OK';
+    $status = true;
+  }
+  $PRECHECK['CUSTOM_CHECKS'][$key] = array(
+      'REQUIRED' => 'CMS OK',
+      'ACTUAL' => $wb_version,
+      'STATUS' => $status
+  );
+}
+else {
+  // check for LEPTON
+  $lepton_version = $database->get_one("SELECT `value` FROM `".TABLE_PREFIX."settings` WHERE `name`='lepton_version'");
+  if (!empty($lepton_version)) {
+    // WebsiteBaker
+    if (!in_array($lepton_version, array('1.1.4', '1.2.0', '1.2.1'))) {
+      $checked = false;
+      $key = "The LEPTON CMS version $wb_version is not approved for extendedWYSIYG!";
+      $status = false;
+    }
+    else {
+      $lepton_version = 'CMS OK';
+      $key = 'CMS Version';
+      $status = true;
+    }
+    $PRECHECK['CUSTOM_CHECKS'][$key] = array(
+        'REQUIRED' => 'CMS OK',
+        'ACTUAL' => $lepton_version,
+        'STATUS' => $status
+    );
+  }
+  else {
+    // can't detect the CMS version
+    $PRECHECK['CUSTOM_CHECKS']['Can\'t detect the CMS version, please contact the support!'] = array(
+        'REQUIRED' => 'CMS OK',
+        'ACTUAL' => '- unknown -',
+        'STATUS' => false
+    );
+  }
+}
+
 
 // check default charset
 $SQL = "SELECT `value` FROM `".TABLE_PREFIX."settings` WHERE `name`='default_charset'";
